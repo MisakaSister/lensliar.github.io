acecaf669f68e22c60f4aeaa7ee7c8956af6a7e0
wengguodong <862422848@qq.com>
2025-07-29 18:53:29 +0800

    style: 调整文章编辑页面的样式和TinyMCE配置
    - 增加编辑容器的最大宽度至1800px，提升页面布局。
    - 调整TinyMCE编辑器的最小高度至1000px，改善内容编辑的可视性。
    - 更新文章编辑页面的脚本链接至最新版本。

--- public/assets/js/article-edit.js
+++ public/assets/js/article-edit.js
@@ -94,7 +94,7 @@ async function initTinyMCEEditor() {
 
     tinyMCEEditor = await tinymce.init({
         selector: '#article-content-editor',
-        height: 800,
+        height: 1000,
         plugins: [
             'advlist autolink lists link image'
         ],
